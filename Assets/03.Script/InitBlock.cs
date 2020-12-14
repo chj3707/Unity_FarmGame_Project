2a78f95ee021e011fb198c715c39e066cda4cf91
chj3707 <chohyunjin4280@gmail.com>
2020-12-14 21:34:17 +0900

    12.14

---
+++ Assets/03.Script/InitBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitBlock : MonoBehaviour
+{
+    public GameObject m_GrassBlock = null;
+    public Transform m_BolockPos = null;
+
+    public int Row = 10; // 행 크기
+    public int Col = 10; // 열 크기
+    void Start()
+    {
+        for (int z = 0; z < Col; z++)
+        {
+            for (int x = 0; x < Row; x++) 
+            {
+                Vector3 posvec = new Vector3(m_BolockPos.position.x + x, 0f, m_BolockPos.position.z + z);
+                GameObject copyobj = GameObject.Instantiate(m_GrassBlock);
+                copyobj.SetActive(true);
+                copyobj.tag = "Grass";
+                copyobj.transform.parent = m_BolockPos;
+                copyobj.transform.Translate(posvec);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
