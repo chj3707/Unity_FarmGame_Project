aa949a360ddaa38cbd24eeb47c799863a5157bce
chj3707 <chohyunjin4280@gmail.com>
2020-12-21 10:39:32 +0900

    씨앗 심기

--- Assets/03.Script/GrowSeed.cs
+++ Assets/03.Script/GrowSeed.cs
@@ -29,24 +29,22 @@ public class GrowSeed : MonoBehaviour
     {
         float tempval = 0f;
         float temptime = 0.01f;
+        Vector3 scale = new Vector3(transform.GetChild(0).localScale.x * 2f, transform.GetChild(0).localScale.y * 5f, transform.GetChild(0).localScale.z * 2f);
 
         PlantState = STATE.PLANT;
         transform.GetChild(0).GetComponent<Renderer>().material.color = Color.green;
 
         while (PlantState == STATE.PLANT)
         {
-            //Vector3 scale = new Vector3(Mathf.Lerp(transform.GetChild(0).localScale.x, transform.GetChild(0).localScale.x * 2, tempval),
-            //    Mathf.Lerp(transform.GetChild(0).localScale.y, transform.GetChild(0).localScale.y * 10, tempval),
-            //    Mathf.Lerp(transform.GetChild(0).localScale.z, transform.GetChild(0).localScale.z * 2, tempval));
-           
-            //transform.GetChild(0).GetComponent<Renderer>().material.color = Color.Lerp(Color.green, Color.red, tempval);
-            //transform.GetChild(0).transform.localScale = scale;
+
+            transform.GetChild(0).GetComponent<Renderer>().material.color = Color.Lerp(Color.green, Color.red, tempval / GrowTime);
+            transform.GetChild(0).transform.localScale = Vector3.Lerp(transform.GetChild(0).localScale, scale, temptime / GrowTime);
 
             tempval += temptime;
 
-            yield return new WaitForSeconds(temptime);      
+            yield return new WaitForSeconds(temptime);
 
-            if(tempval >= GrowTime)
+            if (tempval >= GrowTime)
             {
                 Debug.Log("완료");
                 PlantState = STATE.MATURE;
